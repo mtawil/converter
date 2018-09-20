9fb51f3cad52505db29d74cb576c98c6f4048d41
Mohammad AlTawil <mtawil@users.noreply.github.com>
2018-09-20 12:41:19 +0300

    Update Converter.php

--- src/Converter/Converter.php
+++ src/Converter/Converter.php
@@ -4,32 +4,35 @@
  * @file Converter.php
  * @brief This file contains the Converter class.
  * @details
+ *
  * @author Filippo F. Fadda
+ * @author Mohammad @Taweel
  */
-
 
 /**
  * @brief This namespace contains all the converters.
  */
+
 namespace Converter;
 
 /**
  * @brief This is an abstract converter.
  */
-abstract class Converter {
-  protected $text;
-  protected $id;
+abstract class Converter
+{
+    protected $text;
+    protected $id;
 
-
-  /**
-   * @brief Constructor.
-   * @param string $text The text to be converted.
-   * @param string $id You can provide an identifier which is used in case an exception is raised during the
-   * conversion process.
-   */
-  public function __construct($text, $id = '') {
-    $this->text = $text;
-    $this->id = (string)$id;
-  }
-
+    /**
+     * @brief Constructor.
+     *
+     * @param string $text the text to be converted
+     * @param string $id   you can provide an identifier which is used in case an exception is raised during the
+     *                     conversion process
+     */
+    public function __construct(string $text = null, string $id = null)
+    {
+        $this->text = $text;
+        $this->id = $id;
+    }
 }
